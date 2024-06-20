cfe62504970aea1feb36ec230d6a34a1e1fadfae
Adophilus <uchenna19of@gmail.com>
2024-06-20 13:11:18 +0100

    updated the connector

--- @podfi/webapp/src/routes/-components/wagmi-connector.tsx
+++ @podfi/webapp/src/routes/-components/wagmi-connector.tsx
@@ -1,20 +1,19 @@
 import { WagmiProvider } from 'wagmi'
-import { sepolia } from 'wagmi/chains'
+import { baseSepolia } from 'wagmi/chains'
 import { FunctionComponent, ReactNode } from "react";
 import { createWeb3Modal } from '@web3modal/wagmi/react'
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 import { config } from '@/lib/config'
 
 const metadata = {
-  name: 'Web3Modal',
-  description: 'Podfi',
+  name: 'Podfi',
+  description: 'Connect to Podfi',
   url: 'http://localhost',
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
-
 const wagmiConfig = defaultWagmiConfig({
-  chains: [sepolia],
+  chains: [baseSepolia],
   projectId: config.walletconnect.projectId,
   metadata,
 })
